a93698261adfd847a6330a65422878440ce64f1c
camuHan <mudaya3@gmail.com>
2017-01-11 14:02:17 +0900

    Step Counter initial commit

---
+++ StepCounterApp/src/main/java/com/greenland/stepcounter/StartActivity.java
@@ -0,0 +1,13 @@
+package com.greenland.stepcounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+    }
+}
